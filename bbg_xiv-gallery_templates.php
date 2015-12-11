89c26e6bb5a5637d2399d357ba646a82d6d273c4
magenta-cuda <magenta.cuda@yahoo.com>
2015-12-11 01:48:28 -1000

    add Bootstrap grid template for gallery

--- bbg_xiv-gallery_templates.php
+++ bbg_xiv-gallery_templates.php
@@ -1,1 +1,21 @@
 <!-- Backbone.js templates for the 'gallery' shortcode -->
+
+<!-- Gallery Container Template -->
+<script type="text/html" id="bbg_xiv-template_gallery_container">
+<div class="container bbg_xiv-gallery_container">
+    <div class="row">
+        {{{ data.items }}}
+    </div>
+</div>
+</script>
+<!-- Gallery Item Template -->
+<script type="text/html" id="bbg_xiv-template_gallery_item">
+        <div class="col-sm-6 col-md-4 col-lg-3">
+            <figure class="img-rounded bbg_xiv-gallery_item">
+                <figcaption>{{{ data.post_title }}}</figcaption>
+                <a href="{{{ data.url }}}" target="_blank">
+                    <img src="{{{ data.url }}}">
+                </a>
+            </figure>
+        </div>
+</script>
