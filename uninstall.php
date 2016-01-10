4b64ae3933d45705f66f3af3559710e484d81cb3
magenta-cuda <magenta.cuda@yahoo.com>
2016-01-09 21:55:04 -1000

    delete options on uninstall

---
+++ uninstall.php
@@ -0,0 +1,21 @@
+<?php
+
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
+}
+
+$option_names = [
+    'bbg_xiv_flex_min_width',
+    'bbg_xiv_flex_min_width_for_caption',
+    'bbg_xiv_flex_min_width_for_dense_view',
+    'bbg_xiv_flex_number_of_dense_view_columns',
+    'bbg_xiv_shortcode',
+    'bbg_xiv_table'
+];
+
+foreach ( $option_names as $option_name ) {
+    delete_option( $option_name );
+}
+
+?>
+
