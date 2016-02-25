ff571682bd73ce362369ea4d773637fa1975e66b
magenta-cuda <magenta.cuda@yahoo.com>
2016-02-24 15:05:23 -1000

    fix wrong navbar after option save

--- js/bbg_xiv-gallery.js
+++ js/bbg_xiv-gallery.js
@@ -885,8 +885,13 @@
             outer.hide();
             var inner=gallery.find("div.bbg_xiv-configure_inner");
             inner.hide();
+            // redisplay the "Gallery" view using the new option values
             var gallery=jQuery(this).parents("div.bbg_xiv-gallery");
             bbg_xiv.renderGallery(gallery.find("div.bbg_xiv-gallery_envelope")[0],"Gallery");
+            // reset navbar to "Gallery" view
+            var liSelectView=gallery.find("nav.bbg_xiv-gallery_navbar ul.nav li.bbg_xiv-select_view");
+            var liFirst=liSelectView.find("ul.bbg_xiv-view_menu li").removeClass("active").first().addClass("active");
+            liSelectView.find("a.bbg_xiv-selected_view span").text(liFirst.text());
             jQuery(window).resize();
             e.preventDefault();
         });
